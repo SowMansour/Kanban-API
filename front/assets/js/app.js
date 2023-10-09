76d10c7744ca6b9af7a34130c837530283558670
Mansour Sow <mansoursow@MBP-de-Mansour.lan>
2023-10-09 18:45:10 +0200

    Front Set-up

---
+++ front/assets/js/app.js
@@ -0,0 +1,13 @@
+
+const app = {
+
+    // fonction d'initialisation, lancée au chargement de la page
+    init: function () {
+      console.log('app.init !');
+    }
+  
+  };
+  
+  
+  // on accroche un écouteur d'évènement sur le document : quand le chargement est terminé, on lance app.init
+  document.addEventListener('DOMContentLoaded', app.init );
